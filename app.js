a39363c101e3f5d9e76cc2c09446bc94ff2ede85
Futaku <futaku0811@gmail.com>
2022-01-24 15:57:00 +0900

    finish app

--- app.js
+++ app.js
@@ -1,7 +1,38 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+let booklog = {};
+
+app.use(express.json());
+
+//JSONを記録
+app.post('/booklog', (req, res) => {
+    booklog = req.body
+
+    if (!(booklog.name && booklog.text)) {
+        return res.json({
+            "OK": false,
+            "error": "invalid parameter"
+        });
+    }
+
+    res.json({
+        "OK": true,
+        "booklog":booklog
+    })
+});
+
+//一覧の取得
+app.get("/booklog", (req, res) => {
+    res.json({
+        "OK": true,
+        "booklog": [
+            booklog
+        ]
+    })
+});
 
 //サーバーの起動確認
-app.get('/', (req, res) => res.send('Hello World!'));
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => {
+    console.log('App listening at http://localhost:${port}')
+});
